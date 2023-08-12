744a2773146ed55bba68d3a5cf9c567601a782cc
koenschuite <tse.lao@yahoo.com>
2023-08-12 22:48:32 +0200

    test page

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+export default function page() {
+  return (
+    <div className="flex flex-col gap-6">
+      <span>
+        This is an amazing app and chrome extension that everyone should check
+        out. 
+      </span>
+      <span>Claim your uniquely generate data </span>
+      <span>Unfortunately the following are already taken.</span>
+      <span>¸This tweet helps to showcase our project¸</span>
+      <span>¸CheckMade¸</span>
+    </div>
+  );
+}
